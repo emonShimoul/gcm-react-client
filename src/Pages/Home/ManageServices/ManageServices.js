8b1a7f2d4e92c9dde856ca82784770932e6f6367
emonShimoul <emonshimoul@gmail.com>
2022-07-03 13:34:47 +0600

    added live server link from heroku instead of local node server

--- src/Pages/Home/ManageServices/ManageServices.js
+++ src/Pages/Home/ManageServices/ManageServices.js
@@ -3,13 +3,13 @@ import React, { useEffect, useState } from 'react';
 const ManageServices = () => {
     const [services, setServices] = useState([]);
     useEffect(() => {
-        fetch('http://localhost:5000/services')
+        fetch('https://agile-tor-10091.herokuapp.com/services')
         .then(res => res.json())
         .then(data => setServices(data))
     }, []);
 
     const handleDelete = id => {
-        const url = `http://localhost:5000/services/${id}`;
+        const url = `https://agile-tor-10091.herokuapp.com/services/${id}`;
         fetch(url, {
             method: 'DELETE'
         })
